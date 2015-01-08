255e5e7d39b4b74c30429cb66bef2646e05ab824
MoKuH <jbbernard@openwt.com>
2015-01-08 15:15:53 +0100

    fix i18n translate

--- app/helpers/translate_helper.rb
+++ app/helpers/translate_helper.rb
@@ -16,6 +16,19 @@ module TranslateHelper
     !Rails.env.production?
   end
 
+  def self.is_ok(key,options)
+    missing=false
+    if TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys
+      raise I18n::ArgumentError if key.is_a?(String) && key.empty?
+      begin
+        I18n.translate!(key,options)
+      rescue I18n::MissingTranslationData
+        missing=true
+      end
+    end
+
+    TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys && !missing
+  end
 
 end
 module I18n
@@ -39,23 +52,35 @@ module I18n
       end
     end
   end
+
+  extend(Module.new {
+    def translate(key, options = {},show_key=true)
+      translation=super(key, options)
+      if show_key  && TranslateHelper.is_ok(key,options)
+        if options[:scope].nil?
+          scope=''
+        else
+          scope=options[:scope]+"."
+        end
+        key=scope+key.to_s.gsub(/\./,'--')
+        translation="#{translation}|#{key}|".html_safe
+      end
+      translation
+    end
+    def translate!(key, options={},show_key=false)
+      I18n.translate(key, options.merge(:raise => true),show_key)
+    end
+
+  })
 end
 module ActionView
   # = Action View Translation Helpers
   module Helpers
     module TranslationHelper
-      def translate(key, options = {})
-        missing=false
-        if TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys
-          raise I18n::ArgumentError if key.is_a?(String) && key.empty?
-          begin
-            I18n.translate!(key, :raise => true)
-          rescue I18n::MissingTranslationData
-            missing=true
-          end
-        end
 
-        if TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys && !missing
+
+      def translate(key, options = {})
+        if TranslateHelper.is_ok(key,options)
           options.merge!(:rescue_format => :html) unless options.key?(:rescue_format)
           if html_safe_translation_key?(key)
             html_safe_options = options.dup
@@ -64,20 +89,20 @@ module ActionView
                 html_safe_options[name] = ERB::Util.html_escape(value.to_s)
               end
             end
-            translation = I18n.translate(scope_key_by_partial(key), html_safe_options)
+            translation = I18n.translate(scope_key_by_partial(key), html_safe_options,false)
 
             return translation.respond_to?(:html_safe) ? translation.html_safe : translation
           else
             if options[:scope].nil?
               scope=''
             else
-              scope=options[:scope]
+              scope=options[:scope]+"."
             end
             show_key=scope+key.to_s.gsub(/\./,'--')
 
 
             if  is_trad_array? key
-              result=I18n.translate(scope_key_by_partial(key), options)
+              result=I18n.translate(scope_key_by_partial(key), options,false)
               result_new=Array.new
               result.each do | value|
                 if value.nil?
@@ -88,7 +113,7 @@ module ActionView
               end
               return result_new
             else
-              return I18n.translate(scope_key_by_partial(key), options)+" |#{show_key}|".html_safe
+              return I18n.translate(scope_key_by_partial(key), options,false)+ "|#{show_key}|".html_safe
             end
           end
 
@@ -102,11 +127,11 @@ module ActionView
               end
             end
 
-            translation = I18n.translate(scope_key_by_partial(key), html_safe_options)
+            translation = I18n.translate(scope_key_by_partial(key), html_safe_options,false)
 
             translation.respond_to?(:html_safe) ? translation.html_safe : translation
           else
-            I18n.translate(scope_key_by_partial(key), options)
+            I18n.translate(scope_key_by_partial(key), options,false)
           end
         end
       end
