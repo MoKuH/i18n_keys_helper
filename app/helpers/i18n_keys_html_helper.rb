a339a1aba44681e93cbc351dc4498dd19730958b
MoKuH <jbbernard@openwt.com>
2015-12-11 14:32:30 +0100

    remove module in mobile view

--- app/helpers/i18n_keys_html_helper.rb
+++ app/helpers/i18n_keys_html_helper.rb
@@ -3,7 +3,8 @@ module I18nKeysHtmlHelper
     if TranslateHelper.is_env_acceptable?
       html_to_render="<script>var module_activated=#{TranslateHelper.get_show_keys}</script>"
       html_to_render << raw(javascript_include_tag('translate'))
-      html_to_render << "<div id='toggle_show_key' style='padding:10px;z-index:100;opacity=0.6;width:100px;height:100px;position:fixed;top:10%;right:0;background:#dedede;-webkit-border-top-left-radius: 8px;-webkit-border-bottom-left-radius: 8px;-moz-border-radius-topleft: 8px;-moz-border-radius-bottomleft: 8px;border-top-left-radius: 8px; border-bottom-left-radius: 8px;'>"
+      html_to_render << raw(stylesheet_link_tag('translate'))
+      html_to_render << "<div id='toggle_show_key' >"
       html_to_render << "<p><a href=\"javascript:location.search+='&show_keys=#{!TranslateHelper.get_show_keys}';\">Click here to #{TranslateHelper.get_show_keys ? 'stop displaying' : 'display'} translation keys</a></p>"
       html_to_render << "</div>"
       raw html_to_render
--- lib/i18n_keys_helper/version.rb
+++ lib/i18n_keys_helper/version.rb
@@ -1,3 +1,3 @@
 module I18nKeysHelper
-  VERSION = "1.3.0"
+  VERSION = "1.3.1"
 end
