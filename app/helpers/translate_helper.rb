e858965d0f69c06460e429ba7f4154eb0e3c0cad
jbbernard <jbbernard@openwt.com>
2016-07-04 10:52:50 +0200

    2.0.0

--- app/helpers/translate_helper.rb
+++ app/helpers/translate_helper.rb
@@ -17,109 +17,36 @@ module TranslateHelper
   end
 
   def self.is_ok(key,options)
-    missing=false
-    if TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys
-      raise I18n::ArgumentError if key.is_a?(String) && key.empty?
-      # begin
-      #  I18n.translate!(key,options)
-      # rescue I18n::MissingTranslationData
-      #   missing=true
-      # end
-    end
-
-    TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys && !missing
+    TranslateHelper.is_env_acceptable? && TranslateHelper.get_show_keys
   end
 
 end
-module I18n
-  class MissingTranslation
-    module Base
-      attr_reader :locale, :key, :options
 
-      def initialize(locale, key, options = nil)
-        @key, @locale, @options = key, locale, options.dup || {}
-        options.each { |k, v| self.options[k] = v.inspect if v.is_a?(Proc) }
-      end
-
-      def html_message
-        if Rails.env.production?
-          ''
-        else
-          key  = CGI.escapeHTML titleize(keys.last)
-          path = CGI.escapeHTML keys.join('.')
-          %(<span class="translation_missing" title="translation missing: #{path}">#{key}</span>)
-        end
-      end
-    end
-  end
-
-end
 module ActionView
   # = Action View Translation Helpers
   module Helpers
     module TranslationHelper
-
-
-      def translate(key, options = {})
-        if TranslateHelper.is_ok(key,options)
-          options.merge!(:rescue_format => :html) unless options.key?(:rescue_format)
-          if html_safe_translation_key?(key)
-            html_safe_options = options.dup
-            options.except(*I18n::RESERVED_KEYS).each do |name, value|
-              unless name == :count && value.is_a?(Numeric)
-                html_safe_options[name] = ERB::Util.html_escape(value.to_s)
-              end
-            end
-            translation = I18n.translate(scope_key_by_partial(key), html_safe_options,false)
-
-            return translation.respond_to?(:html_safe) ? translation.html_safe : translation
-          else
-            if options[:scope].nil?
-              scope=''
+      def translate(key, options={})
+        translation=super(key, options.merge(raise: true))
+        if  is_trad_array? key
+          result=I18n.translate(scope_key_by_partial(key), options,false)
+          result_new=Array.new
+          result.each do | value|
+            if value.nil?
+              result_new.push nil
             else
-              scope=options[:scope]+"."
-            end
-            show_key=scope+key.to_s.gsub(/\./,'--')
-
-
-            if  is_trad_array? key
-              result=I18n.translate(scope_key_by_partial(key), options,false)
-              result_new=Array.new
-              result.each do | value|
-                if value.nil?
-                  result_new.push nil
-                else
-                  result_new.push value+ "|#{show_key}|".html_safe
-                end
-              end
-              return result_new
-            else
-              translation = I18n.translate(scope_key_by_partial(key), options,false)
-              if !translation.include?('translation_missing') && !translation.is_a?(Array)
-                translation = translation + "|#{show_key}|".html_safe
-              end
-              translation
+              result_new.push value+ "|#{key}|".html_safe
             end
           end
-
+          return result_new
         else
-          options.merge!(:rescue_format => :html) unless options.key?(:rescue_format)
-          if html_safe_translation_key?(key)
-            html_safe_options = options.dup
-            options.except(*I18n::RESERVED_KEYS).each do |name, value|
-              unless name == :count && value.is_a?(Numeric)
-                html_safe_options[name] = ERB::Util.html_escape(value.to_s)
-              end
-            end
-
-            translation = I18n.translate(scope_key_by_partial(key), html_safe_options,false)
-
-            translation.respond_to?(:html_safe) ? translation.html_safe : translation
-          else
-            I18n.translate(scope_key_by_partial(key), options,false)
-          end
+          translation + "|#{key}|".html_safe
         end
+      rescue I18n::MissingTranslationData
+        key
       end
+
+
       alias :t :translate
       alias :l :localize
 
